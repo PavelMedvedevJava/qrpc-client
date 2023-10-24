dd99cc7e9d6440821991f87cf91311e8c6316329
Pavel_Medvedev <Pavel_Medvedev@epam.com>
2023-10-24 19:44:05 +0200

    create service

--- src/main/java/com/example/grpcclient/GrpcClientApplication.java
+++ src/main/java/com/example/grpcclient/GrpcClientApplication.java
@@ -1,13 +1,33 @@
 package com.example.grpcclient;
 
-import org.springframework.boot.SpringApplication;
+import com.protocol.grpc.PingPongServiceGrpc;
+import com.protocol.grpc.PingPongServiceOuterClass;
+import com.protocol.grpc.PingPongServiceOuterClass.SayPongResponse;
+import com.example.grpcclient.
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.io.IOException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GrpcClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrpcClientApplication.class, args);
-	}
+	public static void main(String[] args)  throws InterruptedException, IOException{
+		ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:9090")
+			.usePlaintext()
+			.build();
 
+		PingPongServiceGrpc.PingPongServiceBlockingStub stub =
+			PingPongServiceGrpc.newBlockingStub(channel);
+
+		PingPongServiceOuterClass.SayPingRequest request =
+			PingPongServiceOuterClass.SayPingRequest.newBuilder()
+				.setPingWord("Ping...")
+				.setGameName("tennis")
+				.build();
+
+		final SayPongResponse response = stub.say(request);
+		System.out.println(response);
+		channel.shutdown();
+	}
 }
